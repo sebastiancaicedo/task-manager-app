3c0ade98f8645afacfb314c11989aa85cb4ac867
Carlos Jimenez <Jimenezgomez@uninorte.edu.co>
2022-05-19 10:56:33 -0500

    Merge con login - login author

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,20 +1,58 @@
-import React from "react";
-import { Button, Form } from "react-bootstrap";
+//import React from "react";
+//import { Button, Form } from "react-bootstrap";
 
-export default function Signin() {
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Alert, Button, Form } from 'react-bootstrap';
+
+import { signIn } from '../api/users';
+import UserContext from '../containers/UserContext';
+
+ export default function SignIn() {
+  const navigate = useNavigate();
+  const { setUser } = useContext(UserContext);
+  const [error, setError] = useState('');
+
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
+      const response = await signIn({
+        email: email.value,
+        password: password.value,
+      });
+       
+       localStorage.setItem('token', response.meta.token)
+       localStorage.setItem('user', JSON.stringify(response.data))
+       
+       navigate('/');
+      }
+
   return (
-    <Form>
-      <Form.Group className="mb-3">
-        <Form.Label>Email address:</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Sign In
-      </Button>
-    </Form>
+    <>
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="text"
+            name="email"
+            placeholder="Enter your username"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" placeholder="submit">
+          Sign In
+        </Button>
+      </Form>
+    </>
   );
 }
