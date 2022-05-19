309e981ddfa458a4e1ba85a8ec0b024fd39c1f9d
Luis Caicedo <sebastiancaicedo@uninorte.edu.co>
2022-05-19 13:37:52 -0500

    Fix conflict archivo http

--- src/api/http.js
+++ src/api/http.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-<<<<<<< HEAD
   baseURL: process.env.REACT_APP_API_URL,
-=======
-  baseURL: `${process.env.REACT_APP_API_URL}`, 
->>>>>>> e6c1c61 (login author)
 });
 
 export default instance;
